99e53505a1425ea31b812e997b4ece11eb3a6b21
jjingofarouk <jjingofaroukk@gmail.com>
2025-04-06 23:47:58 +0300

    Update App.js

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,10 +1,13 @@
-import logo from './logo.svg';
+import logo from './logo.svg'; // Keep your existing logo import (optional)
 import DrugInteractionChecker from './components/DrugInteractionChecker';
+import Navbars from './components/Navbar'; // Import the navbar
+import 'bootstrap/dist/css/bootstrap.min.css'; // Required for Bootstrap styling
 
 function App() {
   return (
     <div className="App">
-< DrugInteractionChecker />
+      <Navbars /> {/* Add the navbar here */}
+      <DrugInteractionChecker /> {/* Your existing component */}
     </div>
   );
 }
