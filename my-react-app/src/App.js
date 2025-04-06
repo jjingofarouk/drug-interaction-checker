35209f4483ee3bff04273c4e1ea283ef49b5e0e5
jjingofarouk <jjingofaroukk@gmail.com>
2025-04-07 00:44:12 +0300

    Update App.js

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import logo from './logo.svg';
-import DrugInteractionChecker from './components/DrugInteractionChecker';
 import Navbars from './components/Navbar';
 import Footer from './components/Footer';
-
+import DrugInteractionChecker from './components/DrugInteractionChecker';
+import './App.css'; // Import the CSS file
 
 function App() {
   return (
@@ -12,17 +11,18 @@ function App() {
       <div className="app-container">
         <Navbars />
         
-        <main>
-        <Routes>
-          <Route path="/" element={<DrugInteractionChecker />} />
-          <Route path="/checker" element={<DrugInteractionChecker />} />
-          {/* Placeholder routes */}
-          <Route path="/about" element={<div>About Page</div>} />
-          <Route path="/drugs" element={<div>Drug Database</div>} />
-          <Route path="/interactions" element={<div>Interactions</div>} />
-          <Route path="/resources" element={<div>Resources</div>} />
-          <Route path="/contact" element={<div>Contact</div>} />
-        </Routes>
+        <main className="main-content">
+          <Routes>
+            <Route path="/" element={<DrugInteractionChecker />} />
+            <Route path="/checker" element={<DrugInteractionChecker />} />
+            <Route path="/about" element={<div>About Page</div>} />
+            <Route path="/drugs" element={<div>Drug Database</div>} />
+            <Route path="/interactions" element={<div>Interactions</div>} />
+            <Route path="/resources" element={<div>Resources</div>} />
+            <Route path="/faq" element={<div>FAQ Page</div>} />
+            <Route path="/contact" element={<div>Contact Page</div>} />
+            <Route path="/donate" element={<div>Donate Page</div>} />
+          </Routes>
         </main>
         
         <Footer />
