88213c5cffab465b770f8e75c1075f9bb4ed0882
jjingofarouk <jjingofaroukk@gmail.com>
2025-04-07 00:31:01 +0300

    Update App.js

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -4,7 +4,7 @@ import logo from './logo.svg';
 import DrugInteractionChecker from './components/DrugInteractionChecker';
 import Navbars from './components/Navbar';
 import Footer from './components/Footer';
-import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
   return (
