18d132fd8a6a844a9d9ebf882eded7146e01e5db
jjingofarouk <jjingofaroukk@gmail.com>
2025-06-22 02:49:44 +0300

    Update App.js

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
-import Footer from './components/Footer';
 import DrugInteractionChecker from './components/DrugInteractionChecker';
 import './App.css'; // Import the CSS file
 
