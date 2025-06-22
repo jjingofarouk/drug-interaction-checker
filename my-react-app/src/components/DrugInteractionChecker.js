442a4dda7ba5c272fa5a43571c7221690ec62394
jjingofarouk <jjingofaroukk@gmail.com>
2025-06-22 16:13:29 +0300

    Update DrugInteractionChecker.js

--- my-react-app/src/components/DrugInteractionChecker.js
+++ my-react-app/src/components/DrugInteractionChecker.js
@@ -4,7 +4,7 @@ import DrugSearchInput from './DrugSearchInput';
 import InteractionCard from './InteractionCard';
 import SuggestionCard from './SuggestionCard';
 import customDrugOptions from './drugOptions.json';
-import customInteractions from './drugInteractionData.json';
+import customInteractions from './druginteractiondata.json';
 import './styles.css';
 
 const DrugInteractionChecker = () => {
